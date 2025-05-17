70510105052b80f6c701679a955e9dae3deab3a2
RoyCad <shaonoutsource.bd@gmail.com>
2025-05-17 18:49:25 +0600

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1747486164914

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7974b689._.js",
+  "static/chunks/src_aaf0c58a._.js"
+],
+    source: "dynamic"
+});
